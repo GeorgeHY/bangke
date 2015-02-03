4ee8bd42e6672127eb852bc0e84b0ccb316e7243
GeorgeHan <hanyangkid@163.com>
2015-02-03 09:22:49 +0800

    Initial Commit

---
+++ bangke/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bangke
+//
+//  Created by iwind on 15/2/3.
+//  Copyright (c) 2015年 BK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ bangke/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  bangke
+//
+//  Created by iwind on 15/2/3.
+//  Copyright (c) 2015年 BK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
